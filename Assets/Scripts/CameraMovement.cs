04422a1fe1eb993601ad4c47ebfedd12f868dbc8
Louis <louis_foy@yahoo.co.uk>
2019-11-13 19:22:44 +0000

    Added impostor texture clipping - no more massive textures!

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,6 +6,7 @@ public class CameraMovement : MonoBehaviour
 {
     public float mouseSensitivity = 1;
     public float speed = 5;
+    public float sprintSpeed = 10;
 
     float horizontalRotation = 0;
     float verticalRotation = 0;
@@ -19,11 +20,13 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : speed;
+
         horizontalRotation += Input.GetAxis("Mouse X") * mouseSensitivity;
         verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        transform.position += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        transform.position += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        transform.position += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * currentSpeed;
+        transform.position += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * currentSpeed;
 
         if (verticalRotation > 180)
         {
