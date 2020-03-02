a88c66390fd91fc9b49560032bcb3feada10e81d
Louis <louis_foy@yahoo.co.uk>
2020-03-02 15:29:42 +0000

    Added quit button to end scene

---
+++ Assets/Scripts/Game/QuitButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// My sole purpose is to quit the game :(
+/// </summary>
+public class QuitButton : MonoBehaviour
+{
+    /// <summary>
+    /// Quits the game. RIP
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
